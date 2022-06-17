49f3656e42bfcd2afb7a1ed02e64bf8a0f590aed
segovelo <gauchosinpingo@gmail.com>
2022-06-17 22:06:49 +0100

    This branch from where to merge

--- src/com/generic/controller/GenericExamplesController.java
+++ src/com/generic/controller/GenericExamplesController.java
@@ -83,17 +83,17 @@ public class GenericExamplesController {
 			System.out.println("\n    i =  " + i);
 		}
 		
-		// This in branch-to-merge-into will merge
+		// This in branch-merge-from  will NOT merge
 		
-		// This in branch-to-merge-into will merge
+		// This in branch-merge-from will NOT merge
 		
-		// This in branch-to-merge-into will NOT merge
+		// This in branch-merge-from will merge
 		
-		// This in branch-to-merge-into will merge
+		// This in branch-merge-from will NOT merge
 		
-		// This in branch-to-merge-into will NOT merge
+		// This in branch--merge-from will  merge
 		
-		// This in branch-to-merge-into will merge
+		// This in branch--merge-from will NOT merge
 		
 		
 	}
